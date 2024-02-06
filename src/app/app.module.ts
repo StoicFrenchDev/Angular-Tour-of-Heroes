d4b38a795d2907d80a5ca9d41b15e8afa8b31700
StoicFrenchDev <richard.benjamin69@yahoo.fr>
2024-02-06 17:28:55 +0100

    Created a hero-details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 
@@ -13,7 +14,8 @@ import { HeroesComponent } from './heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input} from '@angular/core';
+import {NgIf, UpperCasePipe} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {Hero} from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  standalone: true,
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css'],
+  imports: [FormsModule, NgIf, UpperCasePipe],
+})
+
+export class HeroDetailComponent {
+  @Input() hero?: Hero;
+  
+}
