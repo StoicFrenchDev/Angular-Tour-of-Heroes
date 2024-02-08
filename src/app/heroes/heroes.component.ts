5cbeb3dfd8845744fc5b7e97f70a3433e6628429
StoicFrenchDev <richard.benjamin69@yahoo.fr>
2024-02-08 17:25:28 +0100

    Created a few services. No time to explain gotta go sorry futur Ben

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgFor} from '@angular/common';
 import { Hero } from '../hero';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -15,14 +16,13 @@ import { HeroService } from '../hero.service';
   ],
 })
 
-export class HeroesComponent {
-
-
+export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, 
+    private messageService: MessageService) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -35,6 +35,7 @@ export class HeroesComponent {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add('HeroesComponent: Selected hero id=${hero.id}');
   }
 
 }
